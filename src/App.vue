3602a4a92ed98ad2954ed61a4b671d2a6cd46722
Arbuzzzi <safonov.mir@gmail.com>
2019-07-25 15:20:06 +0300

    global

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,26 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <app-calendar @click.native="test($refs)"></app-calendar>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import AppCalendar from './components/AppCalendar'
+
+export default {
+  name: 'App',
+  methods: {
+    test($refs) {
+      console.log($refs);
+    }
+  },
+  components: {
+    AppCalendar
+  },
+  data () {
+    return {
+      //
     }
   }
 }
-</style>
+</script>
